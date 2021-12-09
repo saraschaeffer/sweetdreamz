5807eb54940b255c5ca08fe8108b557610c66ea3
Sara Schaeffer <sara.schaeffer617@gmail.com>
2021-12-09 08:24:59 -0800

    imported example react native svg chart

--- screens/dreamData.js
+++ screens/dreamData.js
@@ -2,22 +2,38 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Button, TouchableOpacity, ScrollView, TextInput } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { Path } from 'react-native-svg'
+import { AreaChart, Grid } from 'react-native-svg-charts'
+import * as shape from 'd3-shape'
 
-export default function tellMeAboutYourDream() {
+class AreaChartExample extends React.PureComponent {
 
-  return (
-    <SafeAreaView>
-      <TextInput
-        placeholder='dream data'
-      />
-    </SafeAreaView>
-  )
+    render() {
+
+        const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+
+        const Line = ({ line }) => (
+            <Path
+                key={'line'}
+                d={line}
+                stroke={'rgb(134, 65, 244)'}
+                fill={'none'}
+            />
+        )
+
+        return (
+            <AreaChart
+                style={{ height: 200 }}
+                data={data}
+                contentInset={{ top: 30, bottom: 30 }}
+                curve={shape.curveNatural}
+                svg={{ fill: 'rgba(134, 65, 244, 0.2)' }}
+            >
+                <Grid/>
+                <Line/>
+            </AreaChart>
+        )
+    }
 }
 
-const styles = StyleSheet.create({
-  input: {
-    marginBottom: 10,
-    paddingHorizontal: 8,
-    paddingVertical: 6
-  }
-})
+export default AreaChartExample
