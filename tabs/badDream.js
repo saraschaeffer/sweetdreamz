4cd89a011dcdfe13249937472e57923d13384a1e
Sara Schaeffer <sara.schaeffer617@gmail.com>
2021-12-12 19:36:04 -0800

    modified bad dream data

--- tabs/badDream.js
+++ tabs/badDream.js
@@ -11,43 +11,97 @@ class BadDream extends React.PureComponent {
 
   render() {
 
-    const pieData = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ];
-    const areaData = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ];
-    const barData = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ];
+    const barData = [2, 1, 3, 1, 6, 7, 5, 8, 5, 3, 3, 2];
     const stackedAreaData = [
       {
-          month: new Date(2021, 0, 1),
+          month: 'January',
           'Blair Waldorf': 3840,
           'Seth Cohen': 1920,
           'Hayley James Scott': 960,
           'Spencer Hastings': 400,
       },
       {
-          month: new Date(2021, 1, 1),
+          month: 'February',
           'Blair Waldorf': 1600,
           'Seth Cohen': 1440,
           'Hayley James Scott': 960,
           'Spencer Hastings': 400,
+      },
+      {
+        month: 'March',
+        'Blair Waldorf': 1600,
+        'Seth Cohen': 1440,
+        'Hayley James Scott': 960,
+        'Spencer Hastings': 400,
+      },
+      {
+        month: 'April',
+        'Blair Waldorf': 1600,
+        'Seth Cohen': 1440,
+        'Hayley James Scott': 960,
+        'Spencer Hastings': 400,
+      },
+      {
+        month: 'May',
+        'Blair Waldorf': 1600,
+        'Seth Cohen': 1440,
+        'Hayley James Scott': 960,
+        'Spencer Hastings': 400,
+      },
+      {
+        month: 'June',
+        'Blair Waldorf': 1600,
+        'Seth Cohen': 1440,
+        'Hayley James Scott': 960,
+        'Spencer Hastings': 400,
+      },
+      {
+        month: 'July',
+        'Blair Waldorf': 1600,
+        'Seth Cohen': 1440,
+        'Hayley James Scott': 960,
+        'Spencer Hastings': 400,
+      },
+      {
+        month: 'August',
+        'Blair Waldorf': 1600,
+        'Seth Cohen': 1440,
+        'Hayley James Scott': 960,
+        'Spencer Hastings': 400,
+      },
+      {
+        month: 'September',
+        'Blair Waldorf': 1600,
+        'Seth Cohen': 1440,
+        'Hayley James Scott': 960,
+        'Spencer Hastings': 400,
+      },
+      {
+        month: 'October',
+        'Blair Waldorf': 1600,
+        'Seth Cohen': 1440,
+        'Hayley James Scott': 960,
+        'Spencer Hastings': 400,
+      },
+      {
+        month: 'November',
+        'Blair Waldorf': 1600,
+        'Seth Cohen': 1440,
+        'Hayley James Scott': 960,
+        'Spencer Hastings': 400,
+      },
+      {
+        month: 'December',
+        'Blair Waldorf': 1600,
+        'Seth Cohen': 1440,
+        'Hayley James Scott': 960,
+        'Spencer Hastings': 400,
       }
   ];
 
         const colors = ['#89CFF0', '#0096FF', '#0F52BA', '#0818A8']
         const keys = ['Blair Waldorf', 'Seth Cohen', 'Hayley James Scott', 'Spencer Hastings']
-        // const svgs = [
-        //     { onPress: () => console.log('apples') },
-        //     { onPress: () => console.log('bananas') },
-        //     { onPress: () => console.log('cherries') },
-        //     { onPress: () => console.log('dates') },
-        // ]
 
-    const Line = ({ line }) => (
-        <Path
-            key={'line'}
-            d={line}
-            stroke={'rgb(134, 65, 244)'}
-            fill={'none'}
-        />
-    );
 
     const Gradient = () => (
         <Defs key={'gradient'}>
@@ -60,33 +114,9 @@ class BadDream extends React.PureComponent {
 
     const randomColor = () => ('#' + (Math.random() * 0xFFFFFF << 0).toString(16) + '000000').slice(0, 7)
 
-    const viewedPieData = pieData
-    .filter(value => value > 0)
-    .map((value, index) => ({
-      value,
-      svg: {
-        fill: randomColor(),
-        onPress: () => console.log('press', index),
-      },
-      key: `pie-${index}`,
-    }))
-
     return (
       <SafeAreaView>
-          <AreaChart
-            style={{ height: 200 }}
-            data={areaData}
-            contentInset={{ top: 30, bottom: 30 }}
-            curve={shape.curveNatural}
-            svg={{ fill: 'rgba(134, 65, 244, 0.2)' }}
-          >
-            <Grid/>
-            <Line/>
-          </AreaChart>
-          {/* <PieChart
-            style={ { height: 200 } }
-            data={ viewedPieData }
-          /> */}
+          <Text>Bad Dreams Trends Over Last Twelve Months</Text>
           <BarChart
               style={ { height: 200 } }
               data={ barData }
@@ -99,7 +129,17 @@ class BadDream extends React.PureComponent {
               <Grid/>
               <Gradient/>
           </BarChart>
-          <Text>Who You Dream About</Text>
+          <Text>{
+            "\n",
+            "\n",
+            "\n",
+            "\n",
+            "\n",
+            "\n",
+            "\n",
+            "\n",
+            "\n"
+          }</Text>
           <StackedAreaChart
                 style={{ height: 200, paddingVertical: 16 }}
                 data={stackedAreaData}
